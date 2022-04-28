5519075be987b3044ba5e25933b2bf74b68ab4de
DMU-2598958 <p2598958@my365.dmu.ac.uk>
2022-04-28 20:37:46 +0100

    1st

--- java/app.js
+++ java/app.js
@@ -11,6 +11,22 @@
 
   menuBtn.addEventListener('click', toggleMenu);
 
+  let slideIndex = 0;
+  showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2500); // Change image every 2 seconds
+};
+
 //When a function is called by an event listener, it is given information about the event. In this case, the event (evt) is set to 'preventDefault', stopping the page from reloading when the submit button of the form is pressed. The display of the 'status' element is also set to 'block.
     submittedForm = (evt) => {
       evt.preventDefault();
