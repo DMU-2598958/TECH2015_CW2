d5b795756a1edc416ae19451a49d10f55f25c555
DMU-2598958 <p2598958@my365.dmu.ac.uk>
2022-04-30 00:41:18 +0100

    again

---
+++ java/app_video.js
@@ -0,0 +1,15 @@
+function changeButton(x) {
+x.classList.toggle("change");
+};
+
+const menuBtn = document.getElementById('burger');
+
+function toggleMenu() {
+  const nav = document.getElementById('menu');
+  nav.classList.toggle('active')
+};
+
+menuBtn.addEventListener('click', toggleMenu);
+
+
+//
